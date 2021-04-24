ae36233ca547be0e42e9c094969ae07816cc5403
Kolbak <kushaevmarat@mail.ru>
2021-04-24 19:33:32 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+'use strict'
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  console.log('MOBILE');
+  let sectionDel = document.querySelector('#skill_sec');
+  sectionDel.remove();
+  let sectionAppend = document.querySelector('#abt_sec');
+  let tmpl = templateSlider.content.cloneNode(true);
+  sectionAppend.after(tmpl);
+
+  $('.carousel-inner').owlCarousel({
+    loop:true,
+    margin:20,
+    // nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+} else {
+  console.log('PC');
+}
